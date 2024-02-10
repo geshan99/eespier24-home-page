0fcf20fa6aff0cfb07b64a23db34257d770101b4
geshan99 <geshansudasinghe@gmail.com>
2024-02-11 01:15:05 +0530

    add some animations

--- src/components/AddSection.jsx
+++ src/components/AddSection.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Carousel } from "react-bootstrap";
 import "./AddSection.css"; // Import CSS for styling
 import ad from "../assets/Advertisements.jpg";
 
 function AddSection() {
+	const addSectionRef = useRef(null);
+	const [isVisible, setIsVisible] = useState(false);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([entry]) => {
+				setIsVisible(entry.isIntersecting);
+			},
+			{
+				root: null,
+				rootMargin: "0px",
+				threshold: 0.5,
+			}
+		);
+
+		observer.observe(addSectionRef.current);
+
+		return () => {
+			observer.unobserve(addSectionRef.current);
+		};
+	}, []);
+
 	return (
-		<div className="add-section">
+		<div
+			className={`add-section ${isVisible ? "visible" : ""}`}
+			ref={addSectionRef}
+		>
 			<h2 className="centered-heading">Meet our Sponsors</h2>
 			<Carousel>
 				<Carousel.Item>
@@ -17,8 +42,7 @@ function AddSection() {
 						src={ad}
 						alt="Second slide"
 					/>
-				</Carousel.Item>
-				{/* Add more Carousel.Item components for additional slides */}
+				</Carousel.Item>{" "}
 			</Carousel>
 		</div>
 	);
