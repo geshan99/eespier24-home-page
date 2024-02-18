6a1a6daccee435ae34805408381480fc85e9b201
janithsamee <janith.samee0@gmail.com>
2024-02-18 06:25:34 +0530

    #added Images

--- src/components/HorizontalScrollingImages.jsx
+++ src/components/HorizontalScrollingImages.jsx
@@ -1,27 +1,89 @@
 // HorizontalScrollingImages.js
 import React from "react";
 import "./HorizontalScrollingImages.css";
-import img from "../assets/Gallery1.png";
-import ascii from "../assets/ASCII.png";
 import dimo from "../assets/DIMO.png";
 import hayleys from "../assets/Hayleys.png";
-import nikini from "../assets/Nikini.png";
-import wibhawa from "../assets/Vibhawa.png";
+import ipd from "../assets/partneships/ipd.jpg";
+import tpl from "../assets/partneships/tpl.jpg";
+import sagt from "../assets/partneships/sagt.jpg";
+import mit from "../assets/partneships/mit.jpg";
+import rma from "../assets/partneships/rma.jpg";
+import ceylex from "../assets/partneships/ceylex.jpg";
+import smartzi from "../assets/partneships/smartzi.jpg";
+import windforce from "../assets/partneships/windforce.jpg";
+import headrogen from "../assets/partneships/headrogen.jpg";
+import promissq from "../assets/partneships/promissq.jpg";
+import nestle from "../assets/partneships/nestle.jpg";
+import dns from "../assets/partneships/dns.jpg";
+import fentons from "../assets/partneships/fentons.jpg";
 
 function HorizontalScrollingImages() {
-	return (
-		<div className="horizontal-scroll-container">
-			<div className="horizontal-scroll-content">
-				{/* Repeat the image elements for the desired number of images */}
-				<img src={ascii} alt="Image 1" />
-				<img src={dimo} alt="Image 2" />
-				<img src={hayleys} alt="Image 3" />
-				<img src={nikini} alt="Image 3" />
-
-				{/* Add more images as needed */}
-			</div>
-		</div>
-	);
+    return (
+        <div className="horizontal-scroll-container">
+            <div className="horizontal-scroll-content">
+                <img src={hayleys} alt="Hayleys Logo" />
+                <img src={dimo} alt="DIMO Logo" />
+                <img src={ipd} alt="IPD Logo" />
+                <img src={tpl} alt="TPL Logo" />
+                <img src={ceylex} alt="Ceylex Logo" />
+                <img src={mit} alt="MIT Logo" />
+                <img src={rma} alt="RMA Logo" />
+                <img src={smartzi} alt="Smartzi Logo" />
+                <img src={sagt} alt="SAGT Logo" />
+                <img src={windforce} alt="Windforce Logo" />
+                <img src={headrogen} alt="Headrogen Logo" />
+                <img src={promissq} alt="Promissq Logo" />
+                <img src={nestle} alt="Nestle Logo" />
+                <img src={dns} alt="DNS Logo" />
+                <img src={fentons} alt="Fentons Logo" />
+                <img src={hayleys} alt="Hayleys Logo" />
+                <img src={dimo} alt="DIMO Logo" />
+                <img src={ipd} alt="IPD Logo" />
+                <img src={tpl} alt="TPL Logo" />
+                <img src={ceylex} alt="Ceylex Logo" />
+                <img src={mit} alt="MIT Logo" />
+                <img src={rma} alt="RMA Logo" />
+                <img src={smartzi} alt="Smartzi Logo" />
+                <img src={sagt} alt="SAGT Logo" />
+                <img src={windforce} alt="Windforce Logo" />
+                <img src={headrogen} alt="Headrogen Logo" />
+                <img src={promissq} alt="Promissq Logo" />
+                <img src={nestle} alt="Nestle Logo" />
+                <img src={dns} alt="DNS Logo" />
+                <img src={fentons} alt="Fentons Logo" />
+                <img src={hayleys} alt="Hayleys Logo" />
+                <img src={dimo} alt="DIMO Logo" />
+                <img src={ipd} alt="IPD Logo" />
+                <img src={tpl} alt="TPL Logo" />
+                <img src={ceylex} alt="Ceylex Logo" />
+                <img src={mit} alt="MIT Logo" />
+                <img src={rma} alt="RMA Logo" />
+                <img src={smartzi} alt="Smartzi Logo" />
+                <img src={sagt} alt="SAGT Logo" />
+                <img src={windforce} alt="Windforce Logo" />
+                <img src={headrogen} alt="Headrogen Logo" />
+                <img src={promissq} alt="Promissq Logo" />
+                <img src={nestle} alt="Nestle Logo" />
+                <img src={dns} alt="DNS Logo" />
+                <img src={fentons} alt="Fentons Logo" />
+                <img src={hayleys} alt="Hayleys Logo" />
+                <img src={dimo} alt="DIMO Logo" />
+                <img src={ipd} alt="IPD Logo" />
+                <img src={tpl} alt="TPL Logo" />
+                <img src={ceylex} alt="Ceylex Logo" />
+                <img src={mit} alt="MIT Logo" />
+                <img src={rma} alt="RMA Logo" />
+                <img src={smartzi} alt="Smartzi Logo" />
+                <img src={sagt} alt="SAGT Logo" />
+                <img src={windforce} alt="Windforce Logo" />
+                <img src={headrogen} alt="Headrogen Logo" />
+                <img src={promissq} alt="Promissq Logo" />
+                <img src={nestle} alt="Nestle Logo" />
+                <img src={dns} alt="DNS Logo" />
+                <img src={fentons} alt="Fentons Logo" />
+            </div>
+        </div>
+    );
 }
 
 export default HorizontalScrollingImages;
--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,43 +1,43 @@
 import "../components/Partners.css";
-import ceylex from "../assets/partneships/celyex.png";
-import ipd from "../assets/partneships/ipd.png";
-import mit from "../assets/partneships/mit.png";
-import rma from "../assets/partneships/rma.png";
-import smartzi from "../assets/partneships/smartzi.png";
-import tpl from "../assets/partneships/tpl.png";
-import sagt from "../assets/partneships/sagt.png";
+import sagt from "../assets/partneships/sagt.jpg";
+import ipd from "../assets/partneships/ipd.jpg";
+import tpl from "../assets/partneships/tpl.jpg";
+import mit from "../assets/partneships/mit.jpg";
+import rma from "../assets/partneships/rma.jpg";
+import ceylex from "../assets/partneships/ceylex.jpg";
+import smartzi from "../assets/partneships/smartzi.jpg";
 
 function Partners() {
-	return (
-		<>
-			<div className="partners-container">
-				<h2 className="heading">Our Partenrs</h2>
-				<div className="row-1">
-					<div className="platinum-container">
-						<h2 className="heading-sub">Platinum Partner</h2>
-						<img src={sagt} />
-					</div>
-					<div className="gold-container">
-						<h2 className="heading-sub">Gold Partners</h2>
-						<img src={sagt} />
-						<img src={sagt} />
-					</div>
-				</div>
-				<div className="row-2">
-					<div className="silver-container">
-						<h2 className="heading-sub">Silver Partner</h2>
-						<img src={sagt} />
-					</div>
-					<div className="bronze-container">
-						<h2 className="heading-sub">Bronze Partners</h2>
-						<img src={sagt} />
-						<img src={sagt} />
-						<img src={sagt} />
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="partners-container">
+                <h2 className="heading">Our Partners</h2>
+                <div className="row-1">
+                    <div className="platinum-container">
+                        <h2 className="heading-sub">Platinum Partner</h2>
+                        <img src={ipd} />
+                    </div>
+                    <div className="gold-container">
+                        <h2 className="heading-sub">Gold Partners</h2>
+                        <img src={sagt} />
+                        <img src={tpl} />
+                    </div>
+                </div>
+                <div className="row-2">
+                    <div className="silver-container">
+                        <h2 className="heading-sub">Silver Partner</h2>
+                        <img src={mit} />
+                    </div>
+                    <div className="bronze-container">
+                        <h2 className="heading-sub">Bronze Partners</h2>
+                        <img src={rma} />
+                        <img src={ceylex} />
+                        <img src={smartzi} />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default Partners;
